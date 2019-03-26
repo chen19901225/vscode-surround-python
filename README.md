79f56fba0ed7c672c582ac4b04f05b5b209ab3a7
chenqinghe <1832866299@qq.com>
2019-03-26 19:34:16 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,56 @@
+# surround-python README
+
+this is a surround extension for python
+
+## Features
+
+surround commands includes: `if`, `ifElse`, `tryFinally`, `tryExcept`, `tryFinallyExcept`, `for`, `with`
+
+keybinding: `alt+shift+z`
+
+
+## Requirements
+
+language: python
+vscode: >= 1.32.0
+
+## Extension Settings
+
+
+
+## Known Issues
+
+Calling out known issues can help limit users opening duplicate issues against your extension.
+
+## Release Notes
+
+Users appreciate release notes as you update your extension.
+
+### 0.0.1
+
+first commit
+
+### 1.0.1
+
+Fixed issue #.
+
+### 1.1.0
+
+Added features X, Y, and Z.
+
+-----------------------------------------------------------------------------------------------------------
+
+## Working with Markdown
+
+**Note:** You can author your README using Visual Studio Code.  Here are some useful editor keyboard shortcuts:
+
+* Split the editor (`Cmd+\` on OSX or `Ctrl+\` on Windows and Linux)
+* Toggle preview (`Shift+CMD+V` on OSX or `Shift+Ctrl+V` on Windows and Linux)
+* Press `Ctrl+Space` (Windows, Linux) or `Cmd+Space` (OSX) to see a list of Markdown snippets
+
+### For more information
+
+* [Visual Studio Code's Markdown Support](http://code.visualstudio.com/docs/languages/markdown)
+* [Markdown Syntax Reference](https://help.github.com/articles/markdown-basics/)
+
+**Enjoy!**
---
+++ src/extension.ts
@@ -0,0 +1,121 @@
+'use strict';
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+import { constants } from 'os';
+interface ISurroundItem {
+    label: string;
+    description?: string;
+    detail?: string;
+    snippet: string;
+    disabled?: boolean;
+}
+interface ISurroundConfig {
+    [key: string]: ISurroundItem
+}
+function getSurroundConfig():ISurroundConfig { // 获取配置
+    let config = vscode.workspace.getConfiguration("surround-python");
+    const items = <ISurroundConfig>config.get("with", {});
+    const custom = <ISurroundConfig>config.get("custom", {});
+    return {...items, ...custom}
+
+}
+
+function getEnableSurroundItems() {
+    const items: ISurroundItem[] = [];
+    const surroundConfig = getSurroundConfig(); // 获取配置
+    Object.keys(surroundConfig).forEach(surroundItemKey => {
+        const surroundItem: ISurroundItem = surroundConfig[surroundItemKey];
+        if(!surroundItem.disabled) {
+            items.push(surroundItem);
+        }
+    })
+    return items;
+}
+function getQuickPickItems(surroundItems: ISurroundItem[]) {
+    const items: vscode.QuickPickItem[] = [];
+    surroundItems.forEach(surroundItem => {
+        if (!surroundItem.disabled) {
+            const {label, description} = surroundItem;
+            items.push({
+                label,
+                description
+            });
+        }
+    })
+    return items;
+}
+function applySurroundItem(key: string) { // 调用命令
+    const config=getSurroundConfig();
+    if(vscode.window.activeTextEditor &&  config[key]) {
+        const surroundItem:ISurroundItem = config[key];
+        vscode.window.activeTextEditor.insertSnippet(new vscode.SnippetString(surroundItem.snippet));
+
+    }
+}
+function applyQuickPick(item: vscode.QuickPickItem, surroundItems: ISurroundItem[]) { // 通过ctrl+shift+p调用命令
+    let activeEditor = vscode.window.activeTextEditor;
+    if (activeEditor && item) {
+        let surroundItem = surroundItems.find(s => item.label == s.label);
+        if (surroundItem) {
+            activeEditor.insertSnippet(new vscode.SnippetString(surroundItem.snippet));
+        }
+    }
+}
+function registerCommands(context: vscode.ExtensionContext) {
+    const Config = getSurroundConfig();
+    vscode.commands.getCommands().then(cmdList => {
+        Object.keys(Config).forEach(key => {
+            const cmdText = `surround-python.with.${key}`;
+            if (cmdList.indexOf(cmdText) == -1) {
+                context.subscriptions.push(
+                    vscode.commands.registerCommand(cmdText, () => {
+                        applySurroundItem(key); // 这个是调用命令？
+                    })
+                )
+            } else {
+                console.warn(`multiple command named ${cmdText}, replacing previous one`);
+            }
+        })
+    });
+}
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+    let quickPickItems: vscode.QuickPickItem[]; // 下拉框的内容
+    let surroundItems: ISurroundItem[] = [];  // 配置的surround列表
+    function update() {
+        surroundItems = getEnableSurroundItems(); // 从配置里面读取surround选项
+        quickPickItems = getQuickPickItems(surroundItems);
+        registerCommands(context);
+    }
+    vscode.workspace.onDidChangeConfiguration(() => {
+        update();
+    })
+    update();
+
+
+    // Use the console to output diagnostic information (console.log) and errors (console.error)
+    // This line of code will only be executed once when your extension is activated
+    console.log("surround-python",'Congratulations, your extension "surround-python" is now active!');
+
+    // The command has been defined in the package.json file
+    // Now provide the implementation of the command with  registerCommand
+    // The commandId parameter must match the command field in package.json
+    let disposable = vscode.commands.registerCommand('surround-python.with', () => {
+        // The code you place here will be executed every time your command is executed
+        vscode.window.showQuickPick(quickPickItems).then(item => {
+            if(item) {
+                applyQuickPick(item, surroundItems);
+            }
+        })
+        // Display a message box to the user
+        // vscode.window.showInformationMessage('Hello World!');
+    });
+
+    context.subscriptions.push(disposable);
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {
+}
